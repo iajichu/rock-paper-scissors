53aa796a238deda64a59cae8d31e6632b48c1758
iajichu <101339747+iajichu@users.noreply.github.com>
2022-09-20 14:38:42 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,32 @@
+input.onGesture(Gesture.Shake, function () {
+    myhand = randint(1, 3)
+    if (myhand == 1) {
+    	
+    }
+})
+let myhand = 0
+radio.setGroup(203)
+let scissorsimage = images.createImage(`
+    # # . . #
+    # # . # .
+    . . # . .
+    # # . # .
+    # # . . #
+    `)
+let rockimage = images.createImage(`
+    # # # # #
+    # . . . #
+    # . . . #
+    # . . . #
+    # # # # #
+    `)
+let paperimage = images.createImage(`
+    . . . . .
+    . # # # .
+    # # # # #
+    # # # # #
+    . . . . .
+    `)
+basic.forever(function () {
+	
+})
